13b8948c0cc543f62a68fa32c4692ba16a2e93c6
Sai Cheemalapati <saicheems@google.com>
2014-06-19 12:33:34 -0700

    Update Emergency Dialer to match Dialer (5/7).
    Emergency dialer style updated to match style in Dialer and
    InCallUI.
    Moved all dialpad resources to PhoneCommon.
    
    Change-Id: Ia3b937c12fc133a04c10ba842df9e89012ba331d

--- Android.mk
+++ Android.mk
@@ -13,11 +13,14 @@
 # limitations under the License.
 
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS := optional
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_RESOURCE_FILES := $(addprefix $(LOCAL_PATH)/, res)
 
-LOCAL_MODULE := com.android.phone.shared
+LOCAL_PACKAGE_NAME := com.android.phone.common
 
-include $(BUILD_STATIC_JAVA_LIBRARY)
+include $(BUILD_PACKAGE)
+include $(CLEAR_VARS)
